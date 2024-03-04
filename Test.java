9acbd8f77b8e8badb2b842571cb5e55578d56c8a
Nikki <146110256+Nikki-Tl@users.noreply.github.com>
2024-03-04 22:37:49 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,7 +9,23 @@
 
 public class Test {
     
-
+    public static void main(String[] args) {
+            
+            
+    
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Name: ");
+            String tempName = sc.next();
+    
+    
+            Player jamal = new Player(tempName, 0, null, null, 0, 0, 0, 0, 0, 0);
+    
+            System.out.println(jamal.getName());
+    
+    
+    
+    
+    }
 
 
 
