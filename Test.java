7427962d2143f8cd50ea1f9b6b22af133e787590
KaneFRGU <146110708+KaneFRGU@users.noreply.github.com>
2024-03-05 12:36:59 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -12,6 +12,13 @@ public class Test {
     public static void main(String[] args) {
             
             
+        Race Human = new Race(0, 0, 1, 1, 0, 1);
+        Race Elf = new Race(0, 0, 2, 1, 0, 0);
+        Race Orc = new Race(2, 2, 1, -2, 0, 0);
+        Race Dwarf = new Race(1, 0, 0, 1, 0, 1);
+        Race Dragonborn = new Race(2, 1, 1, -2, 0, 1);
+        Race Halfling = new Race(-1, -1, 1, 2, 0, 2);
+        Race Tiefling = new Race(0, -1, 0, 1, 1, 2);
     
             Scanner sc = new Scanner(System.in);
             System.out.println("Name: ");
