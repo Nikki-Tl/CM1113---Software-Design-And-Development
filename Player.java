32bb6e0aaf27c7fb8681e0a7ce08b92503d1de82
Nikki <146110256+Nikki-Tl@users.noreply.github.com>
2024-03-12 15:28:38 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,125 +1,124 @@
 
 /*
-    ----------------------------------------------------------------
-
-    
-
-    ----------------------------------------------------------------
+*   ----------------------------------------------------------------
+*   @author: Nikola Tluczek
+*   @author:
+*   @author:
+*   @author:
+*
+*   ----------------------------------------------------------------
 */
 
 public class Player {
     
     /* Initialising Variables */
+    /* NT: Possible change for age variable as range is set in Race? */
     private String name;
-    private String race;
-    private String type;
+    private Race race;
+    private Type type;
 
-    private int hp;
-    private int str;
-    private int dex;
-    private int intel;
-    private int wis;
-    private int cha;
+    private int totalStatHp;
+    private int totalStatStr;
+    private int totalStatDex;
+    private int totalStatInt;
+    private int totalStatWis;
+    private int totalStatCha;
 
 
     /* Constructor */
-    public Player(String name, String race, String type, int hp, int str, int dex, int intel, int wis, int cha) {
+    public Player(String name, Race race, Type type, int totalStatHp, int totalStatStr, int totalStatDex, int totalStatInt, int totalStatWis, int totalStatCha) {
         this.name = name;
         this.race = race;
         this.type = type;
-        this.hp = hp;
-        this.str = str;
-        this.dex = dex;
-        this.intel = intel;
-        this.wis = wis;
-        this.cha = cha;
+
+        this.totalStatHp = totalStatHp;
+        this.totalStatStr = totalStatStr;
+        this.totalStatDex = totalStatDex;
+        this.totalStatInt = totalStatInt;
+        this.totalStatWis = totalStatWis;
+        this.totalStatCha = totalStatCha;
     }
- 
+
+
     /* Getters and Setters per Variable */
     /* name */
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    /* N: cannot have getters and setters for race abd type? */
     /* race */
-    public String getRace() {
+    public Race getRace() {
         return race;
     }
-
-    public void setRace(String race) {
+    public void setRace(Race race) {
         this.race = race;
     }
 
     /* type */
-    public String getType() {
+    public Type getType() {
         return type;
     }
-
-    public void setType(String type) {
+    public void setType(Type type) {
         this.type = type;
     }
 
     /* hp */
-    public int getHp() {
-        return hp;
+    public int getTotalStatHp() {
+        return totalStatHp;
     }
-
-    public void setHp(int hp) {
-        this.hp = hp;
+    public void setTotalStatHp(int totalStatHp) {
+        this.totalStatHp = totalStatHp;
     }
 
     /* str */
-    public int getStr() {
-        return str;
+    public int getTotalStatStr() {
+        return totalStatStr;
     }
-
-    public void setStr(int str) {
-        this.str = str;
+    public void setTotalStatStr(int totalStatStr) {
+        this.totalStatStr = totalStatStr;
     }
 
     /* dex */
-    public int getDex() {
-        return dex;
+    public int getTotalStatDex() {
+        return totalStatDex;
     }
-
-    public void setDex(int dex) {
-        this.dex = dex;
+    public void setTotalStatDex(int totalStatDex) {
+        this.totalStatDex = totalStatDex;
     }
 
     /* intel */
-    public int getIntel() {
-        return intel;
+    public int getTotalStatInt() {
+        return totalStatInt;
     }
-
-    public void setIntel(int intel) {
-        this.intel = intel;
+    public void setTotalStatInt(int totalStatInt) {
+        this.totalStatInt = totalStatInt;
     }
 
     /* wis */
-    public int getWis() {
-        return wis;
+    public int getTotalStatWis() {
+        return totalStatWis;
     }
-
-    public void setWis(int wis) {
-        this.wis = wis;
+    public void setTotalStatWis(int totalStatWis) {
+        this.totalStatWis = totalStatWis;
     }
 
     /* cha */
-    public int getCha() {
-        return cha;
+    public int getTotalStatCha() {
+        return totalStatCha;
+    }
+    public void setTotalStatCha(int totalStatCha) {
+        this.totalStatCha = totalStatCha;
     }
 
-    public void setCha(int cha) {
-        this.cha = cha;
-    }
 
-
-
-
-
+    
+   
+ 
+    
+    
 
 }
