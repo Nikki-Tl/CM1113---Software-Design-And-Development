961dc6c33c1e86a2e883672be122e5303a63d58f
Nikki <146110256+Nikki-Tl@users.noreply.github.com>
2024-03-12 15:46:24 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -20,8 +20,18 @@ public class Test {
         Race Dragonborn = new Race(15, 80, 2, 1, 1, -2, 0, 1);
         Race Halfling = new Race(20, 250, -1, -1, 1, 2, 0, 2);
         Race Tiefling = new Race(20, 100, 0, -1, 0, 1, 1, 2);
-                
-
+        
+        /* Type setup */
+        /* N: change depending on what we do for arrays */
+        Type Barbarian = new Type(2, 3, 0, -2, 0, 0, null, null, null);
+        Type Bard = new Type(-1, -1, 1, 1, 0, 3, null, null, null);
+        Type Cleric = new Type(-2, -2, 1, 3, 2, 1, null, null, null);
+        Type Druid = new Type(0, -1, 0, 1, 3, 0, null, null, null);
+        Type Fighter = new Type(1, 1, 1, 0, 0, 0, null, null, null);
+        Type Paladin = new Type(3, 2, -1, -1, -1, 1, null, null, null);
+        Type Ranger = new Type(-2, 1, 3, 0, 1, 0, null, null, null);
+        Type Rouge = new Type(0, 0, 3, 0, -1, 1, null, null, null);
+        Type Sorcerer = new Type(-1, -2, 1, 3, 2, 0, null, null, null);
 
 
 
