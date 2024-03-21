27188fe67005985ee608024a66a3c08228f15f44
Nikki <146110256+Nikki-Tl@users.noreply.github.com>
2024-03-21 21:32:09 +0000

    added notes

--- CharacterCreator.java
+++ CharacterCreator.java
@@ -13,12 +13,13 @@
 
 public class CharacterCreator extends Stats {
 
+    // N - initialising variables
     private String name;
     private String raceName;
     private String professionName;
 
 
-
+    // N - constructor with the superclass Stats
     public CharacterCreator(String name, String raceName, String professionName, int health, int strength, int dexterity, int intelligence, int wisdom, int charisma) {
         super(health, strength, dexterity, intelligence, wisdom, charisma);
         
@@ -27,31 +28,38 @@ public class CharacterCreator extends Stats {
         this.professionName = professionName;
     }
 
-
+    // N - getter and setters 
     public String getName() {
+
         return name;
     }
-
     public void setName(String name) {
+
         this.name = name;
+
     }
 
     public String getRaceName() {
+
         return raceName;
     }
-
     public void setRaceName(String raceName) {
+
         this.raceName = raceName;
+
     }
 
     public String getProfessionName() {
+
         return professionName;
     }
-
     public void setProfessionName(String professionName) {
+
         this.professionName = professionName;
+
     }
 
+    // N - method to display name, race and profession alongside stats
     public void displayStats() {
         System.out.println("Name: " + getName());
         System.out.println("Race: " + getRaceName());
