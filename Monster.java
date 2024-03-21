7db3851135ff8d049806578dd62cfe6d73f2aed1
FinnyBoi789 <146110224+FinnyBoi789@users.noreply.github.com>
2024-03-21 15:33:45 +0000

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,16 +1,48 @@
 public class Monster extends Player{
     private String monsterDescription;
     private String monsterName;
+    private Boolean isDead;
 
-    public Monster(int statHp, int statStr, int statDex, int statInt,
-            int statWis, int statCha, String monsterDescription, String monsterName) {
-        super(statHp, statStr, statDex, statInt, statWis, statCha);
+    public Monster(int health, int strength, int dexterity, int intelligence, int wisdom, int charisma, String monsterDescription, String monsterName, Boolean isDead) {
+        super(health, strength, dexterity, intelligence, wisdom, charisma);
         this.monsterDescription = monsterDescription;
+        this.monsterName = monsterName;
+        this.isDead = isDead;
+    }
+
+    public String getMonsterDescription() {
+        return monsterDescription;
+    }
+
+    public void setMonsterDescription(String monsterDescription) {
+        this.monsterDescription = monsterDescription;
+    }
+
+    public String getMonsterName() {
+        return monsterName;
+    }
+
+    public void setMonsterName(String monsterName) {
         this.monsterName = monsterName;
     }
 
+    public Boolean getIsDead() {
+        return isDead;
+    }
+
+    public void setIsDead(Boolean isDead) {
+        this.isDead = isDead;
+    }
+
     public String toString(){
-        String displayMonster = "Name: " + monsterName + "\n" + "Description: " + monsterDescription + "\n";             
+        String displayMonster = "Name: " + monsterName + "\n" + 
+                                "Description: " + monsterDescription + "\n" +
+                                "Health Points: " + health + "\n" +
+                                "Strength: " + strength + "\n" + 
+                                "Dexterity: " + dexterity + "\n" +
+                                "Intelligence: " + intelligence + "\n" +
+                                "Wisdom: " + wisdom + "\n" +
+                                "Charisma: " + charisma + "\n";             
             return displayMonster;
         }
 }
