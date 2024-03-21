84f75b4d560883347473b26b9186ac7454a0a1f8
Nikki <146110256+Nikki-Tl@users.noreply.github.com>
2024-03-21 21:50:27 +0000

    Update Race.java

--- Race.java
+++ Race.java
@@ -8,38 +8,42 @@
   *
   */
 
-public class Profession extends Stats {
 
-    // N - professionName variable initialised
-    private String professionName;
+public class Race extends Stats {
+
+    // N - raceName variable inistialised
+    private String raceName;
 
 
-    // N - constructor with the superclass Stats
-    public Profession(String professionName, int health, int strength, int dexterity, int intelligence, int wisdom, int charisma) {
+
+
+
+    // N - constructor with superclass of Stats added in
+    public Race(String raceName, int health,  int strength, int dexterity, int intelligence, int wisdom, int charisma) {
         super(health, strength, dexterity, intelligence, wisdom, charisma);
-        
-        this.professionName = professionName;
+
+        this.raceName = raceName;
 
     }
 
 
-    // N - getter and setter for professionName
-    public String getProfessionName() {
+    // N - getter and setter for race name
+    public String getRaceName() {
 
-        return professionName;
+        return raceName;
     }
-    public void setProfessionName(String professionName) {
+    public void setRaceName(String raceName) {
 
-        this.professionName = professionName;
+        this.raceName = raceName;
 
     }
 
-    // N - method to display profession name with stats
+
+    // N - method to display race name along with stats
     public void displayStats() {
 
         System.out.println();
         super.displayStats();
 
     }
-
 }
