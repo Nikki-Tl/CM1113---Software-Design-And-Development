3f4e996b829a5f4ad63713729a3268b39f3e4288
Nikki <146110256+Nikki-Tl@users.noreply.github.com>
2024-03-21 15:32:57 +0000

    Create CharacterCreator.java

---
+++ CharacterCreator.java
@@ -0,0 +1,52 @@
+public class CharacterCreator extends Player {
+
+    private String name;
+    private String raceName;
+    private String professionName;
+
+
+
+    public CharacterCreator(String name, String raceName, String professionName, int health, int strength, int dexterity, int intelligence, int wisdom, int charisma) {
+        super(health, strength, dexterity, intelligence, wisdom, charisma);
+        
+        this.name = name;
+        this.raceName = raceName;
+        this.professionName = professionName;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRaceName() {
+        return raceName;
+    }
+
+    public void setRaceName(String raceName) {
+        this.raceName = raceName;
+    }
+
+    public String getProfessionName() {
+        return professionName;
+    }
+
+    public void setProfessionName(String professionName) {
+        this.professionName = professionName;
+    }
+
+    public void displayStats() {
+        System.out.println("Name: " + getName());
+        System.out.println("Race: " + getRaceName());
+        System.out.println("Profession: " + getProfessionName());
+        super.displayStats();
+    
+    }
+
+
+    
+}
